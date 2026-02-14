ad632a030dfb353a7d13cfd5cb80903b69399599
DH <leedonghun10032@gmail.com>
2026-02-14 11:27:47 +0000

    동기화

---
+++ project/engine/system/memory.cpp
@@ -0,0 +1,11 @@
+typedef char _1byte;
+typedef unsigned short _2byte;
+typedef unsigned int _4byte;
+typedef unsigned long _8byte;
+
+struct {
+    _2byte status;
+    _2byte debuff;
+    _2byte buff;    
+    _4byte action;
+} Unit;
