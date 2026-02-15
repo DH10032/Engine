cf59b0efc14e15b967328a83c15d6ff107c032f9
DH <leedonghun10032@gmail.com>
2026-02-15 05:01:44 +0000

    merged by LDH

--- project/engine/rendering/render.cpp
+++ project/engine/rendering/render.cpp
@@ -24,53 +24,61 @@ namespace GameGraphicApi {
         int Bright;
     } window_info;
 
-
-    bool Create_window(window_info* info) {
-
-        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-            std::cout << "SDL Init Error: " << SDL_GetError() << std::endl;
-            return false;
-        }
-
-        if (!(IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))) {
-            std::cout << "SDL_image Init Error: " << IMG_GetError() << std::endl;
-            return false;
-        }
-
-        info->window = SDL_CreateWindow(
+    /**
+     * @date 26/2/15
+     * @author 이동훈
+     * @brief SDL 비디오 시스템 초기화 및 윈도우 생성
+     * 
+     * @param[in] title 생성될 윈도우의 제목
+     * @param[in] flags 생성될 SDL_Window의 설정 플래그
+     * @note - 기본 flag: SDL_WINDOW_SHOWN
+     * @note - OpenGL flag: SDL_WINDOW_OPENGL
+     * @note - 자세한 플래그는 SDL_WindowFlags 참고  
+     * 
+     * @param[out] renderer 윈도우에 연결될 SDL_Renderer 객체 포인터
+     * 
+     * @exception renderer혹은 window가 생성되지 않을 경우 에러문구 및 함수 종료
+    */
+    void Create_window(GameGraphicApi::window_info* info, SDL_WindowFlags flags){
+        std::cout << "run : " << info->window_name << std::endl;
+        SDL_Init(SDL_INIT_VIDEO);
+        SDL_Window* window = SDL_CreateWindow(
             info->window_name,
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
             800, 600,
-            SDL_WINDOW_SHOWN
+            flags
         );
 
+        info->window = window;
         if (!info->window) {
             std::cout << "Window Creation Error: " << SDL_GetError() << std::endl;
-            return false;
+            return;
         }
 
-        info->renderer = SDL_CreateRenderer(info->window, -1, SDL_RENDERER_ACCELERATED);
-
+        info->renderer = SDL_CreateRenderer(window, -1, 0);
         if (!info->renderer) {
             std::cout << "Renderer Creation Error: " << SDL_GetError() << std::endl;
-            return false;
+            return;
         }
 
-        // 하얀색 배경화면
         SDL_SetRenderDrawColor(
             info->renderer,
             info->Red,
-            info->Green,
             info->Blue,
+            info->Green,
             info->Bright
         );
-
-        return true;
     }
 
-
-    SDL_Texture* Path_to_Texture(SDL_Renderer* renderer, const char* path) {
+    /**
+    * @brief jpg,png,bmp를 SDL_Texture*로 변환해주는 함수
+    * @param[in] SDL_Renderer* window에 렌러딩할 객체들
+    * @param[in] char* path Texture로 바꿀 path
+    * @param[out] SDL_Texture* 경로를 변환한 Texture
+    * @return 확장자가 정상적일 경우, Texture, 확장자가 지원하지 않을 경우, NULL
+    */
+    SDL_Texture* Path_to_Texture(SDL_Renderer* renderer, char* path){
 
         std::filesystem::path p(path);
         std::string extension = p.extension().string();
@@ -118,6 +126,19 @@ namespace GameGraphicApi {
 
 
 int main() {
+    GameGraphicApi::window_info window_setting {
+        .window_name = "test_game",
+        .window = NULL,
+        .renderer = NULL,
+        .Red = 255,
+        .Green = 255,
+        .Blue = 255,
+        .Bright = 255
+    };
+    GameGraphicApi::Create_window(&window_setting, SDL_WINDOW_SHOWN);
+    SDL_Surface* BMP = IMG_Load("../../character/bug1.png");
+    SDL_Texture* IMG = SDL_CreateTextureFromSurface(window_setting.renderer, BMP);
+    SDL_FreeSurface(BMP);
 
     GameGraphicApi::window_info window_setting{
         "test_game",
@@ -125,10 +146,6 @@ int main() {
         nullptr,
         255, 255, 255, 255
     };
-
-    if (!GameGraphicApi::Create_window(&window_setting))
-        return -1;
-
 
     SDL_Texture* IMG =
         GameGraphicApi::Path_to_Texture(
