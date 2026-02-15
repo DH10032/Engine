6b3f0c473917cdc4aa815ee46fbaf6213e7813c8
tobit1024 <61526826+tobit1024@users.noreply.github.com>
2026-02-15 14:19:54 +0000

    오류 임시 제거

--- project/engine/rendering/render.cpp
+++ project/engine/rendering/render.cpp
@@ -102,18 +102,6 @@ namespace GameGraphicApi{
         return Texture;
     }
 
-    // 캐릭터 이미지 전체를 
-    void Create_Anmation(json unit_json){
-        json Data = {};
-        SDL_Texture* mem = (SDL_Texture*)
-        for (int i=0; i<unit_json["parts"].size();i++){
-            SDL
-            for (int j=0; j<unit_json["parts"][i].size();j++){
-
-            }
-        }
-    }
-
     /**
     * @brief 현재 미완 사용하지 말 것
     */
