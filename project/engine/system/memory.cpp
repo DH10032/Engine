0f34fae85179ab45fe11ed49a79b10a790b21e83
DH <leedonghun10032@gmail.com>
2026-02-16 14:55:16 +0000

    Makefile 완성

--- project/engine/system/memory.cpp
+++ project/engine/system/memory.cpp
@@ -7,20 +7,20 @@ typedef unsigned long _8byte;
 
 // padding 고려해서 제거하기
 
-struct {
-    _4byte status;      // HP 및 공격력 등
-    _2byte debuff;      // 디버프 관리
-    _2byte buff;        // 디버프 관리
-    _4byte action;      // 행동 id
-} Unit;
+// struct {
+//     _4byte status;      // HP 및 공격력 등
+//     _2byte debuff;      // 디버프 관리
+//     _2byte buff;        // 디버프 관리
+//     _4byte action;      // 행동 id
+// } Unit;
 
-json Load_Action_json(){
-    std::ifstream file("../../data/action.json");
-    json data = json::parse(file);
-    return data;
-}
+// json Load_Action_json(){
+//     std::ifstream file("../../data/action.json");
+//     json data = json::parse(file);
+//     return data;
+// }
 
-int main(){
-    json a = Load_Action_json();
-    std::cout << a.dump(2);
-}
+// int main(){
+//     json a = Load_Action_json();
+//     std::cout << a.dump(2);
+// }
