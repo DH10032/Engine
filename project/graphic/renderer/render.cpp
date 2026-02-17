954204ed20a78fb9dfe9be78e422d123439875aa
tobit1024 <61526826+tobit1024@users.noreply.github.com>
2026-02-17 07:34:19 +0000

    폴더 대격변

--- project/graphic/renderer/render.cpp
+++ project/graphic/renderer/render.cpp
@@ -10,9 +10,6 @@ g++ render.cpp -o render -lSDL2 -lSDL2_image -lSDL2_ttf
 
 */
 
-// 색깔 
-SDL_Color BLACK = { 0, 0, 0 };
-
 
 
 
@@ -145,34 +142,4 @@ namespace CreateAssets{
         SDL_Texture* texture;
         SDL_Rect dst; // (x,y,tile_width,tile_height)
     }img;
-}
-
-namespace CreateText{
-    SDL_Renderer* TXT_renderer; // init에서 한번 호출 후 쭉 쓸수 있게
-
-    TTF_Font* bitlimFont; // 많아지면 배열로 관리?
-
-    void TTF_start(SDL_Renderer* renderer){
-        TXT_renderer = renderer;
-        if (TTF_Init() == -1){
-            std::cout << "Failed to load font." << std::endl;
-        } else {
-            bitlimFont = TTF_OpenFont("../../assets/fonts/bitlim.ttf", 30); // 30 뭔지 모르겠음
-            if(bitlimFont == NULL) {
-                printf("Could not open font! (%s)\n", TTF_GetError());
-            }
-        }
-
-    }
-
-    void TTF_end(){
-        TTF_CloseFont(bitlimFont); // 많아지면 for 돌릴 예정
-    }
-
-    void TTF_Create(const char* txtContent, int x, int y, int w, int h){ // 글 내용, x,y,width,height (앞으로 간단히 그릴 수 있도록 함수 제작)
-        SDL_Rect Rct = { x, y, w, h };
-        SDL_Surface* FSurface = TTF_RenderText_Blended(bitlimFont, txtContent, BLACK); // 일단 색상은 검정 통일, 이후 수정 예정
-        SDL_Texture* FTexture = SDL_CreateTextureFromSurface(TXT_renderer, FSurface);
-        SDL_RenderCopy(TXT_renderer, FTexture, NULL, &Rct);
-    }
 }
