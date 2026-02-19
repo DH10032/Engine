e5aee0510b7a20034eee7f53d061427cc1887944
DH <leedonghun10032@gmail.com>
2026-02-19 14:51:22 +0000

    진행 중

--- Engine/graphic/animation/animation.cpp
+++ Engine/graphic/animation/animation.cpp
@@ -2,18 +2,22 @@
 
 
 namespace Animation{
+
     void Assemble_Texture(SDL_Renderer* renderer, SDL_Texture* picture ,std::vector<Parts> p){
         for(Parts part:p){
             SDL_RenderCopyEx(
-                renderer,       // 렌더러
-                picture,        // 텍스처
-                &(part.src),           // 소스 영역 (NULL = 전체)
-                &(part.dst),      // 목적지 영역
-                part.angle,        // 각도 (도 단위, 시계방향)
-                &(part.center),    // dst기준 상대 좌표
-                SDL_FLIP_NONE   // 뒤집기 옵션
+                renderer,           // 렌더러
+                picture,            // 텍스처
+                &(part.src),        // 소스 영역 (NULL = 전체)
+                &(part.dst),        // 목적지 영역
+                part.angle,         // 각도 (도 단위, 시계방향)
+                &(part.center),     // 회전중심(dst기준 상대 좌표)
+                SDL_FLIP_NONE       // 뒤집기 옵션
             );
         }
+    }
+
+    void update_Texture(std::vector<Parts> p){
 
     }
 }
